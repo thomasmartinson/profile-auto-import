da918c091ea1f3020918cdfbc3069afaf2a0e64f
= <tmm2@princeton.edu>
2020-10-14 11:31:47 -0400

    begin Monster scraping

--- monsterImport.js
+++ monsterImport.js
@@ -1,3 +1,71 @@
 $(document).ready(function(){ 
-    alert("it worked!")
+    chrome.runtime.onMessage.addListener(
+        function(message, sender, sendResponse){
+            switch(message.type) {
+                case "scrape":
+                    let xml_str = scrape();
+                    sendResponse(xml_str);
+                    break;
+                case "import":
+                    break;
+            }
+        }
+    );
 });
+
+
+// extracts all info from profile page
+function scrape() {
+    // make sure user has a candidate selected
+    if ($(".candidate-profile-pane div").hasClass("candidate-profile-empty")) {
+        alert("Please select a candidate before importing.");
+        return "";
+    }
+    
+    // make sure user is on "Resume" tab
+    if ($("#candidateProfile #__tab_1").hasClass("tab--inactive")) {
+        alert("Please click on the \"Resume\" tab in the candidate's profile before importing.");
+        return "";
+    }
+
+    // TODO parse the resume text
+    
+    let info = {};
+    
+    // full name
+    info["name"] = $("#candidateProfile .candidate-name div span:first").text();
+
+    // last activity
+    info["last_activity"] = "";
+
+    // last resume update
+    info["resume_updated"] = $(".candidate-resumeupdated-text").text();
+    
+    // email address
+    info["email"] = $(".has-candidate-contact-block:last-child #contact-legend-detail").text().trim();
+
+    // phone number
+    info["phone"] = $(".has-candidate-contact-block:first-child").text().replaceAll(/[^0-9]/g, "");
+    
+    // full address
+    info["address"] = "";
+
+    // work documents
+    info["work_docs"] = "";
+
+    // approx first 10 lines of resume
+    info["resume_preview"] = "";
+
+    // name of the resume file
+    info["resume_file"] = "";
+
+    // build XML string
+    let xml_str = "";
+    for (let item in info) {
+        console.log(`${item}: ${info[item]}`);
+        xml_str += `<${item}>${info[item]}</${item}>\n`
+    }
+    xml_str = `<data>\n${xml_str}</data>` 
+
+    return xml_str;
+}
