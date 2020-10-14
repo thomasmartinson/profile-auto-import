1b422550cc82793b75b2de22e9200be36e08400b
= <tmm2@princeton.edu>
2020-10-14 15:34:46 -0400

    parse info from resume

--- monsterImport.js
+++ monsterImport.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
                     sendResponse(xml_str);
                     break;
                 case "import":
+                    // TODO
                     break;
             }
         }
@@ -28,7 +29,17 @@ function scrape() {
         return "";
     }
 
-    // TODO parse the resume text
+    // parse the resume text
+    const max_length = 1000;
+    let resume_text = "";
+    $("#resume-frame").contents().find("p").each(function() {
+        // approx first 10 lines of resume
+        if (resume_text.length > max_length) {
+            return;
+        }
+
+        resume_text += $(this).text() + "\n";
+    });
     
     let info = {};
     
@@ -43,20 +54,34 @@ function scrape() {
     
     // email address
     info["email"] = $(".has-candidate-contact-block:last-child #contact-legend-detail").text().trim();
+    if (!info.email && REGEXES.email.test(resume_text)) {
+        info.email = resume_text.match(REGEXES.email)[0];
+    }
 
     // phone number
     info["phone"] = reformat_phone($(".has-candidate-contact-block:first-child").text());
+    if (!info.phone) {
+        info.phone = reformat_phone($(".has-candidate-contact-block:nth-child(2)").text());
+    }
+    if (!info.phone && REGEXES.phone.test(resume_text)) {
+        info.phone = reformat_phone(resume_text.match(REGEXES.phone)[0]);
+    }
     
     // full address
-    info["address"] = "";
+    if (REGEXES.address.test(resume_text)) {
+        info["address"] = resume_text.match(REGEXES.address)[0];
+    } else {
+        info["address"] = $("#candidateProfile .candidate-location").text();
+    }
 
     // work documents
     info["work_docs"] = "";
 
     // approx first 10 lines of resume
-    info["resume_preview"] = "";
+    info["resume_preview"] = escape_html(resume_text);
 
     // name of the resume file
+    // TODO access with downloads API
     info["resume_file"] = "";
 
     // build XML string
